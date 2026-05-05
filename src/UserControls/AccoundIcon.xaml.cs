a989bca11f839c491719eeb8fd8c9f7acb05b2f5
K D <karim.debbabi4@gmail.com>
2026-05-05 16:00:54 +0200

    commit between

--- src/UserControls/AccoundIcon.xaml.cs
+++ src/UserControls/AccoundIcon.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MarkIt.UserControls
 {
@@ -20,9 +21,32 @@ namespace MarkIt.UserControls
     /// </summary>
     public partial class AccoundIcon : UserControl
     {
+        private bool hover = false;
         public AccoundIcon()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromTicks(50000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (hover)
+            {
+                ElliepseBody.Margin = new Thickness(0, 0, 0, 0);
+            }
+        }
+
+        private void RectHover_MouseEnter(object sender, MouseEventArgs e)
+        {
+            hover = true;
+        }
+
+        private void RectHover_MouseLeave(object sender, MouseEventArgs e)
+        {
+            hover = false;
         }
     }
 }
