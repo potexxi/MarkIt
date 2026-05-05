00b8f05e9892e90285f7f58d86d68808af355a12
K D <karim.debbabi4@gmail.com>
2026-05-05 15:53:55 +0200

    worked on icon-animations

--- src/UserControls/InformationIcon.xaml.cs
+++ src/UserControls/InformationIcon.xaml.cs
@@ -10,8 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MarkIt.UserControls
 {
@@ -20,9 +22,64 @@ namespace MarkIt.UserControls
     /// </summary>
     public partial class InformationIcon : UserControl
     {
+        private double position { get; set; } = 0;
+        private bool hover = false;
+        private bool up = true;
         public InformationIcon()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromTicks(50000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (hover)
+            {
+                EllispeI.Margin = new Thickness(0, position, 0, 0); // new thinkness von chatgpt
+                if(up)
+                {
+                    position += 0.25;
+                    if (position > 3.5)
+                        up = false;
+                }
+                else
+                {
+                    position -= 0.25;
+                    if (position < -3.5)
+                        up = true;
+                }
+            }
+            if((position <= -0.3 || position >= 0.3 )&& !hover)
+            {
+                EllispeI.Margin = new Thickness(0, position, 0, 0); // new thinkness von chatgpt
+                if (up)
+                {
+                    position += 0.5;
+                    if (position > 3.5)
+                        up = false;
+                }
+                else
+                {
+                    position -= 0.5;
+                    if (position < -3.5)
+                        up = true;
+                }
+            }
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            hover = true;
+            EllispeI.Fill = Brushes.White;
+        }
+
+        private void Rectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            hover = false;
+            EllispeI.Fill = Brushes.Black;
         }
     }
 }
--- src/UserControls/SettingsIcon.xaml.cs
+++ src/UserControls/SettingsIcon.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MarkIt.UserControls
 {
@@ -20,9 +24,41 @@ namespace MarkIt.UserControls
     /// </summary>
     public partial class SettingsIcon : UserControl
     {
+        private double rotation { get; set; } = 0;
+        private bool hover = false;
         public SettingsIcon()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromTicks(50000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (hover)
+            {
+                rotation += 1;
+                Rect3.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5); // chatgpt weil ursprung von rect 3 und rect 4 nicht passen
+                Rect4.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5); // chat gpt ende
+                Rect1.RenderTransform = new RotateTransform(rotation);// chatgpt this line promt: how do I make a rotation in wpf backend
+                Rect2.RenderTransform = new RotateTransform(rotation + 90);
+                Rect4.RenderTransform = new RotateTransform(rotation + 45);
+                Rect3.RenderTransform = new RotateTransform(rotation + 45);
+            }
+        }
+
+        private void RectSettingsIcon_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Rect1.Fill = Brushes.White;
+            hover = true;
+        }
+
+        private void RectSettingsIcon_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Rect1.Fill = Brushes.Black;
+            hover = false;
         }
     }
 }
