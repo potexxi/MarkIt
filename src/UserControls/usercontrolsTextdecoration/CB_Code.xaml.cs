1fe17d453d97d37984161c9a6ef852ac41ad1c93
K D <karim.debbabi4@gmail.com>
2026-05-24 23:20:39 +0200

    created CB for worksheet
    work in progress designing the buttons

---
+++ src/UserControls/usercontrolsTextdecoration/CB_Code.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarkIt.UserControls.usercontrolsTextdecoration
+{
+    /// <summary>
+    /// Interaktionslogik für CB_Code.xaml
+    /// </summary>
+    public partial class CB_Code : UserControl
+    {
+        public CB_Code()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/UserControls/usercontrolsTextdecoration/CB_StrikeThrough.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarkIt.UserControls.usercontrolsTextdecoration
+{
+    /// <summary>
+    /// Interaktionslogik für CB_StrikeThrough.xaml
+    /// </summary>
+    public partial class CB_StrikeThrough : UserControl
+    {
+        public CB_StrikeThrough()
+        {
+            InitializeComponent();
+        }
+    }
+}
