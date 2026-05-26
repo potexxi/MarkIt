b3b1ed450264d0cb288e106c1014d540aac64c11
K D <karim.debbabi4@gmail.com>
2026-05-26 16:24:31 +0200

    Colors On Buttons
    I applied the color theme to the buttons

--- src/UserControls/usercontrolsTextdecoration/CB_Italic.xaml.cs
+++ src/UserControls/usercontrolsTextdecoration/CB_Italic.xaml.cs
@@ -9,7 +9,9 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,9 +22,45 @@ namespace MarkIt.UserControls.usercontrolsTextdecoration
     /// </summary>
     public partial class CB_Italic : UserControl
     {
+        static public Brush hovercolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Foreground); // chatgpt this specific line might be used very often
+        static public Brush defaultcolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Textcolor);
+        static public Brush Backgroundcolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+        public bool animation = false;
+
         public CB_Italic()
         {
             InitializeComponent();
+        }
+
+        public void updateSettings()
+        {
+            animation = MainWindow.GeneralSettings.iconAnimations;
+            RectBackground.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Textcolor);
+            RectBackground.Stroke = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+
+            Rect_I.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+            Ellipse_I.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+        }
+
+        private void CB_Hitbox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void CB_Hitbox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (animation)
+            {
+                RectBackground.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Foreground);
+                RectBackground.Stroke = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+
+                Rect_I.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Textcolor);
+                Ellipse_I.Fill = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Textcolor);
+            }
+        }
+
+        private void CB_Hitbox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            updateSettings();
         }
     }
 }
