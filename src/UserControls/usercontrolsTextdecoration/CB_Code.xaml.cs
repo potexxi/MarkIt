80d9b72dae3dc0fc254848c4905f8635cb0f47f1
K D <karim.debbabi4@gmail.com>
2026-05-26 09:01:11 +0200

    Changed Layout + Small updates

--- src/UserControls/usercontrolsTextdecoration/CB_Code.xaml.cs
+++ src/UserControls/usercontrolsTextdecoration/CB_Code.xaml.cs
@@ -20,9 +20,29 @@ namespace MarkIt.UserControls.usercontrolsTextdecoration
     /// </summary>
     public partial class CB_Code : UserControl
     {
+        static public Brush hovercolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Foreground); // chatgpt this specific line might be used very often
+        static public Brush defaultcolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.Textcolor);
+        static public Brush Backgroundcolor = (Brush)new BrushConverter().ConvertFromString(MainWindow.GeneralSettings.currentColorTheme.HoverColor);
+
+
         public CB_Code()
         {
             InitializeComponent();
+        }
+
+        private void CB_Hitbox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CB_Hitbox_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void CB_Hitbox_MouseLeave(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
